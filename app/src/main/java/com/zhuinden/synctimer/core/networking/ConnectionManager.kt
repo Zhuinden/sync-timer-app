cf2b9ef267940b4c343c87e7761f8fc58095656e
zhuinden <zhuinden@gmail.com>
2019-06-11 16:28:23 +0200

    Fix APK provided in root + add a real license + add screenshot to readme

--- app/src/main/java/com/zhuinden/synctimer/core/networking/ConnectionManager.kt
+++ app/src/main/java/com/zhuinden/synctimer/core/networking/ConnectionManager.kt
@@ -1,9 +1,22 @@
-/*
- * Created in 2019 by Gabor Varadi
+/* Copyright (c) 2019, Gabor Varadi
+ * All rights reserved.
  *
- * You may not use this file except in compliance with the license.
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
+ * conditions are met:
  *
- * The license says: "please don't release this app as is under your own name, thanks".
+ * - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided with the distribution.
+ * - The binary form of the software cannot be redistributed for commercial use under the name of a separate entity
+ * when the binary form is built from using all included source files, or any of the full and complete compilation modules of the software are used as a submodule of another software.
+ * - Prior written permission must be given in order to use the name of its author or other project contributors
+ * for endorsing or promoting products derived from this software.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package com.zhuinden.synctimer.core.networking
 
--- app/src/main/java/com/zhuinden/synctimer/utils/RxUtils.kt
+++ app/src/main/java/com/zhuinden/synctimer/utils/RxUtils.kt
@@ -1,9 +1,13 @@
 /*
- * Created in 2019 by Gabor Varadi
+ * Created in 2019, Gabor Varadi
  *
- * You may not use this file except in compliance with the license.
+ * This file, the code contained within the file, and any parts of the code can be freely distributed or used in any purposes.
+ * Any code/software derived from this file does not need to retain this notice.
  *
- * The license says: "please don't release this app as is under your own name, thanks".
+ * IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package com.zhuinden.synctimer.utils
 
